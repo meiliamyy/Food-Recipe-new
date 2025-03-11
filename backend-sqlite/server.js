1a02cdaaeb3d1f377daac148cde7300ee3382d99
meiliamyy <meiliarahmi8@gmail.com>
2025-03-11 23:05:31 +0700

    add backend sqlite at food recipe

--- backend-sqlite/server.js
+++ backend-sqlite/server.js
@@ -1,2 +1,16 @@
+//import modul 
 const express = require('express');
-const sqlite3 = 
+const sqlite3 = sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+
+
+//inisialisasi aplikasi
+const app = express();
+const port = 5000;
+
+
+//menggunakan middleware 
+app.use(cors());
+app.use(express.json());
+
+//membuka atau membuat database
